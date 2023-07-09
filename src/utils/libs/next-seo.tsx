19dc18d6f5503aa2406141b638e69dc0082f8569
Dwi Nugroho <adwinugroho1@gmail.com>
2023-07-09 22:06:16 +0700

    chore: set seo

--- src/utils/libs/next-seo.tsx
+++ src/utils/libs/next-seo.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 import { NextSeo, DefaultSeo, NextSeoProps, DefaultSeoProps } from 'next-seo';
 
+const origin = process.env.ORIGIN || 'http://127.0.0.1:3000';
+
 export const seo = {
   title: 'Open Graph Generator by krafan',
   description:
@@ -8,10 +10,31 @@ export const seo = {
   titleTemplate: '%s | KRAFAN'
 };
 
+export const ogImage = `${origin}/api?${new URLSearchParams({
+  title: seo.title,
+  description: seo.description,
+  tag: 'Open Graph Image Generator',
+  image: origin + '/img/krafan-400x400.jpg'
+})}`;
+
 export const useNextSeoConfig = (
   props: NextSeoProps | DefaultSeoProps
 ): NextSeoProps | DefaultSeoProps => ({
   ...seo,
+  openGraph: {
+    type: 'website',
+    url: origin,
+    title: seo.title,
+    description: seo.description,
+    images: [
+      {
+        url: ogImage,
+        width: 1200,
+        height: 630,
+        alt: seo.title
+      }
+    ]
+  },
   ...props
 });
 
