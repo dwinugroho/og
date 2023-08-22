afb83209e73b4a365c4a615cb195d2f39f8bc7bb
DwiNugroho <adwinugroho1@gmail.com>
2023-08-22 19:38:50 +0700

    feat: reslicing og image

--- src/pages/api/index.tsx
+++ src/pages/api/index.tsx
@@ -1,37 +1,36 @@
 import { ImageResponse } from '@vercel/og';
 import { NextRequest } from 'next/server';
 
-import { verifyImage } from '@/utils/http/file';
+import clsx from 'clsx';
 
 export const config = {
   runtime: 'edge'
 };
 
+// fonts
 export const hubot400 = fetch(
   new URL(
     '@/assets/fonts/hubot-sans/TTF/Hubot-Sans-Regular.ttf',
     import.meta.url
   )
 ).then((res) => res.arrayBuffer());
+export const mona700 = fetch(
+  new URL('@/assets/fonts/mona-sans/TTF/Mona-Sans-Bold.ttf', import.meta.url)
+).then((res) => res.arrayBuffer());
 
+// classes
 const bgclass: any = {
   light: 'bg-blue-50',
-  dark: 'bg-[#202532]'
+  dark: 'bg-neutral-900'
 };
-
 const textclass: any = {
   light: 'text-black',
   dark: 'text-white'
 };
-
 const descriptionclass: any = {
   light: 'text-gray-700',
   dark: 'text-slate-300'
 };
-
-export const mona700 = fetch(
-  new URL('@/assets/fonts/mona-sans/TTF/Mona-Sans-Bold.ttf', import.meta.url)
-).then((res) => res.arrayBuffer());
 
 export default async function handler(req: NextRequest) {
   try {
@@ -45,73 +44,58 @@ export default async function handler(req: NextRequest) {
       description: searchParams.get('description') ?? '',
       tag: searchParams.get('tag') ?? '',
       image: searchParams.get('image') ?? '',
-      theme: (searchParams.get('theme') || '').toLowerCase() ?? 'dark'
+      theme:
+        (searchParams.get('theme') || '').toLowerCase() === 'light'
+          ? 'light'
+          : 'dark'
     };
 
     return new ImageResponse(
       (
         <div
           style={{ fontFamily: 'Hubot' }}
-          tw={`flex flex-col justify-center w-full h-full relative p-20 ${
+          tw={clsx(
+            'flex flex-row-reverse justify-center',
+            'w-full h-full relative',
             bgclass[query.theme]
-          }`}
+          )}
         >
-          <div tw="flex absolute bottom-0 right-0">
-            <svg
-              width="822"
-              height="328"
-              viewBox="0 0 822 328"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+          {query.image && (
+            <div tw={clsx('flex flex-1 h-full')}>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src={query.image}
+                alt={query.title}
+                style={{
+                  width: '600px',
+                  height: '630px',
+                  objectFit: 'cover'
+                }}
+              />
+            </div>
+          )}
+          <div tw={clsx('flex flex-1 h-full relative')}>
+            <div
+              style={{ transform: 'skewX(-16deg)' }}
+              tw={clsx(
+                'flex',
+                'absolute top-0 left-1 bottom-0 -right-25',
+                'bg-cyan-700'
+              )}
+            ></div>
+            <div
+              style={{ transform: 'skewX(-16deg)' }}
+              tw={clsx(
+                'flex',
+                'absolute top-0 left-0 bottom-0 -right-24',
+                bgclass[query.theme]
+              )}
+            ></div>
+            <div
+              tw={clsx(
+                'flex flex-col items-start justify-center w-full h-full p-16'
+              )}
             >
-              <g filter="url(#filter0_f_2_1424)">
-                <path
-                  d="M283.696 316.909C112.66 355.226 152.642 458.089 160.184 486.188L906.814 503.215L910.465 158.118C892.442 145.326 846.627 139.958 807.549 220.827C758.701 321.913 497.49 269.012 283.696 316.909Z"
-                  fill="url(#paint0_linear_2_1424)"
-                />
-              </g>
-              <defs>
-                <filter
-                  id="filter0_f_2_1424"
-                  x="0.778717"
-                  y="0.700623"
-                  width="1059.69"
-                  height="652.514"
-                  filterUnits="userSpaceOnUse"
-                  color-interpolation-filters="sRGB"
-                >
-                  <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                  <feBlend
-                    mode="normal"
-                    in="SourceGraphic"
-                    in2="BackgroundImageFix"
-                    result="shape"
-                  />
-                  <feGaussianBlur
-                    stdDeviation="75"
-                    result="effect1_foregroundBlur_2_1424"
-                  />
-                </filter>
-                <linearGradient
-                  id="paint0_linear_2_1424"
-                  x1="851.6"
-                  y1="170.876"
-                  x2="180"
-                  y2="425"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop offset="0.0385781" stop-color="#0A51A5" />
-                  <stop offset="1" stop-color="#C54FC8" />
-                  <stop offset="1" stop-color="#821A84" />
-                </linearGradient>
-              </defs>
-            </svg>
-          </div>
-          <div
-            style={{ zIndex: '999' }}
-            tw="flex items-center h-full w-full relative"
-          >
-            <div tw="flex flex-col items-start w-[0%] grow">
               {query.tag && (
                 <span
                   style={{ fontFamily: 'Mona' }}
@@ -137,21 +121,6 @@ export default async function handler(req: NextRequest) {
                 </p>
               )}
             </div>
-            {query.image && (
-              <div tw="flex items-center justify-center h-[400px] w-[400px] ml-16">
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                  src={query.image}
-                  alt={query.title}
-                  style={{
-                    width: '100%',
-                    height: '100%',
-                    objectFit: 'cover'
-                  }}
-                  tw="rounded-lg"
-                />
-              </div>
-            )}
           </div>
         </div>
       ),
